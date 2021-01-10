c6466ddf2f1f28d2e10e97abdfb516cee7f4fbce
Drake974 <70998537+Drake974@users.noreply.github.com>
2021-01-10 05:28:15 +0400

    jquery fonctionnel

--- ressources/maquette/js/media.js
+++ ressources/maquette/js/media.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
-    if(window.innerWidth <= 414){
+    if(window.innerWidth <= 426){
         $('#input-group').removeClass('input-group');
-        
+        $('#footer-center').removeClass('justify-content-between');
+        $('#footer-center').addClass('justify-content-center');
+   
     } else {
         $('#input-group').addClass('input-group'); 
+        $('#footer-center').removeClass('justify-content-center');
+        $('#footer-center').addClass('justify-content-between');
     }
 });
+
+// $(document).ready(function(){
+//     if(window.innerWidth <= 426){
+//        $("#input-group").toggleClass("input-group"); 
+        
+//     }
+// });
+
+// $(document).ready(function(){
+// alert(window.innerWidth);
+// });
+//le message apparait bien
