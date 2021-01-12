b27a981e94b09c6c7b3fd4e592913dc1177752b2
Drake974 <70998537+Drake974@users.noreply.github.com>
2021-01-12 09:08:17 +0400

    changement de variable dans le fichier js pour afficher le range

--- ressources/maquette/js/range.js
+++ ressources/maquette/js/range.js
@@ -1,8 +1,8 @@
-var slider = document.getElementById("price-range");
-var output = document.getElementById("price-value");
-output.innerHTML = slider.value;
-slider.oninput = function() {
-output.innerHTML = this.value;
+var sliderRent = document.getElementById("price-range");
+var outputRent = document.getElementById("price-value");
+outputRent.innerHTML = sliderRent.value;
+sliderRent.oninput = function() {
+outputRent.innerHTML = this.value;
 }
 
 var slider = document.getElementById("availability-range");
