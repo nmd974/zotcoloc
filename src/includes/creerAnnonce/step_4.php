da4b8ed2edcc3da9bca49e98a63f4f8fbb9dd14d
olivier lebon <olivier@lebon.dev>
2021-01-13 07:31:59 +0400

    Update step_4.php

--- src/includes/creerAnnonce/step_4.php
+++ src/includes/creerAnnonce/step_4.php
@@ -1,6 +1,38 @@
 <!--STEP 4-->
 <div id="bloc_step_4" class="unshow_step">
-   
+
+<h4 class="mt-3">Quel est le profil idéal pour cette colocation ? :</h4>
+
+    <div>
+        
+        <p >Informez les candidatures sur le type de profil que vous recherchez.</p>
+        
+        <div class="d-flex align-items-end">
+            <p class="d-flex align-items-end">plutôt</p>
+            
+        
+
+            <div class="btn" role="group" aria-label="Basic radio toggle button group">
+                <input type="radio" class="btn-check" name="homme" id="homme" autocomplete="off">
+                <label class="btn btn-outline-primary border-0" for="homme">
+                <i class="fa fa-male text-dark" aria-hidden="true"></i>
+                Homme</label>
+
+                <input type="radio" class="btn-check" name="femme" id="femme" autocomplete="off">
+                <label class="btn btn-outline-primary border-0" for="femme">
+                <i class="fa fa-female text-dark" aria-hidden="true"></i>
+                Femme</label>
+
+                <input type="radio" class="btn-check" name="indifferent" id="btnradio3" autocomplete="off">
+                <label class="btn btn-outline-primary border-0" for="btnradio3">
+                <i class="fa fa-users text-dark" aria-hidden="true"></i>
+                indifférent</label>
+            </div>
+        </div>
+
+    </div>
+    
+
 
     <!--button validation inscription-->
     <div class="col-12 text-end my-4">
