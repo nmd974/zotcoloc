d9f62fdc1f8dcb409518902e70f16b8fbe4b75c5
olivier lebon <olivier@lebon.dev>
2021-01-13 13:52:59 +0400

    Update seconnecter.php

--- src/pages/seconnecter.php
+++ src/pages/seconnecter.php
@@ -6,7 +6,7 @@
         <h2 class=" mt-3 mb-5 text-center"> Je me Connecte </h2>
 
         <div class=" form-group row row-cols-md-2 row-cols-1 pb-4">
-          <label for="Email" class="form-label col-sm-2 ">Email address</label>
+          <label for="Email" class="form-label col-sm-2 ">Adresse Email:</label>
           <input type="text" name="Email" class="form-control" aria-describedby="emailHelp" required
             pattern=" [a-z0-9 ._% + -] + @ [a- z0-9 .-] + \. [az] {2,} $ " 
             
@@ -14,7 +14,7 @@
         </div>
 
         <div class="form-group row row-cols-md-2 row-cols-1">
-          <label for="Password" class="form-label col-md-3">Mot passe</label>
+          <label for="Password" class="form-label">Mot de passe:</label>
           <input type="password" class="form-control" id="Password" onkeyup="check()" required
             pattern=" ^ (? =. * [az] ) (? =. * [AZ]) (? =. * \ D) (? =. * [@ $!% *? &]) [A-Za-z \ d @ $!% *? &] { 8,} $" 
             value="<?php if(isset($_POST['PasswordUser'])){echo $_POST['PasswordUser'];}?>">
