775e265f1d2981026b462a4ffdb12decf038cc69
Drake974 <70998537+Drake974@users.noreply.github.com>
2021-01-20 19:05:47 +0400

    limitation de la taille de l'image

--- src/pages/editAnnoncePage.php
+++ src/pages/editAnnoncePage.php
@@ -27,7 +27,7 @@
                         <?php else :?>
                         <?php foreach($images[1] as $image):?>
                         <div class="carousel-item active">
-                            <img src="../images/<?= $image->libelle_photo ?>" class="d-block w-100" alt="room">
+                            <img src="../images/<?= $image->libelle_photo ?>" class="d-block w-100" alt="room" style="height:190px;">
                         </div>
                         <?php endforeach; ?>
                         <?php endif; ?>
