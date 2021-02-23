0b7a6f1d7881f796c7ca180d1be5bc3df116aa69
nmd974 <jha.payet@gmail.com>
2021-02-23 11:02:05 +0400

    ajout de l'inscription proprietaire

---
+++ src/js/signupProprietaire.js
@@ -0,0 +1,51 @@
+document.getElementById('ajouter_proprietaire').addEventListener("click", (e)=>{
+    
+    e.preventDefault();
+    //Verification si email en doublon
+    
+    if($('#proprietaire_inscription').valid()){
+        var email = document.getElementById('email').value;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 202) {
+                document.getElementById("valid_email").innerHTML = this.responseText;
+                document.getElementById("valid_email").style.color = 'red';
+                document.getElementById('email').classList.add('is-invalid');             
+            }
+            if(this.readyState == 4 && this.status == 200){
+                document.getElementById("valid_email").style.color = 'green';
+                document.getElementById('email').classList.add('is-valid');
+                document.getElementById("valid_email").innerHTML = this.responseText;
+                
+                //On gère l'affichage du bouton
+                let dot = document.getElementById('dot_1');
+                dot.innerHTML = `<i class="fa fa-check text-white" aria-hidden="true"></i>`;
+                dot.classList.remove('unvalid_step');
+                dot.classList.add('valid_step');
+                
+                //On gère l'affichage du bloc de step avec les classes
+                blockStepEl1.classList.remove('show_step');
+                blockStepEl1.classList.add('unshow_step');
+                blockStepEl2.classList.remove('unshow_step');
+                blockStepEl2.classList.add('show_step');
+                
+                //On redefini le dot vers le point suivant
+                let dotNext = document.getElementById('dot_2');
+                dotNext.innerHTML = `<i class="fa fa-hourglass-start text-white" aria-hidden="true"></i>`;
+                titleStep.innerHTML = `Etape 2/5:<span class="text-green"> Informations générales</span>`;
+                timeLineEl.style.width = `25%`;
+                window.scrollTo(0,0);
+                
+                //Gestion de l'ajout dans le recap des donnees saisies
+                let el = document.querySelector('input[name="email"]');
+                let recapEl = document.getElementById(`email_recap`);
+                let content = document.createElement('p');
+                content.innerHTML = `${el.value}`;
+                recapEl.append(content);
+                // validator.destroy();
+            }
+        };
+        xmlhttp.open("POST", "http://127.0.0.1:8000/src/controllers/utilisateurs/getEmail.php?email=" + email, true);
+        xmlhttp.send();
+    };
+});
--- src/pages/inscriptionProprietaire.php
+++ src/pages/inscriptionProprietaire.php
@@ -41,13 +41,62 @@
         <div class="alert alert-danger mb-2"><?=  $inscriptionValide[1] ?></div>
     <?php endif;?>
     
-    <form method="POST" enctype="multipart/form-data" id="formulaire_inscription">
-        <?php require_once(dirname(__DIR__).'/includes/signupFormProprietaire/step_1.php');?>
+    <form method="POST" enctype="multipart/form-data" id="proprietaire_inscription">
+        
+<!--STEP 1-->
+<div class="show_step">
+    <!--Nom-->
+    <div class="col-md-12">
+        <label for="nom_proprietaire" class="form-label">Nom*</label><br>
+        <input type="text" name="nom_proprietaire" class="form-control" id="nom_proprietaire" value="<?php if(isset($_POST['nom_proprietaire'])){
+            echo $_POST['nom_proprietaire'];
+        }?>">
+    </div>
+    <!--Prenom-->
+    <div class="col-md-12">
+        <label for="prenom_proprietaire" class="form-label">Prénom*</label><br>
+        <input type="text" name="prenom_proprietaire" class="form-control" id="prenom_proprietaire" value="<?php if(isset($_POST['prenom_proprietaire'])){
+            echo $_POST['prenom_proprietaire'];
+        }?>">
+    </div>
+    <!--téléphone-->
+    <div class="col-md-12">
+        <label for="telephone" class="form-label">Téléphone*</label>
+        <input type="tel" class="form-control" id="telephone" name="telephone" value="<?php if(isset($_POST['telephone'])){
+            echo $_POST['telephone'];
+        }?>">
+    </div>
+    <!--Email-->
+    <div class="col-md-12">
+        <label for="email" class="form-label">Email*</label>
+        <input type="email" class="form-control" id="email" name="email" value="<?php if(isset($_POST['email'])){
+            echo $_POST['email'];
+        }?>">
+    </div>
+    <!--mot passe-->
+    <div class="col-md-12">
+        <label for="password" class="form-label">Mot de passe*</label>
+        <input type="password" class="form-control" id="password" name="password">
+    </div>
+    <!--confirmer mot passe-->
+    <div class="col-md-12">
+        <label for="confirm_password" class="form-label">Confirmer le mot de passe*</label>
+        <input type="password" class="form-control" id="confirm_password" name="confirm_password">
+    </div>
+    <div class="col-md-12 mt-4">
+        *Champs obligatoires
+    </div>
+    <!--button validation inscription-->
+    <div class="col-12 text-end my-4">
+        <button type="submit" class="btn w-55 bg-green text-white mr-5" id="ajouter_proprietaire" name="ajouter_proprietaire">Je m'inscris</button>
+    </div>
+</div>
     </form>
 
 </div>
 
 <?php require_once(dirname(__DIR__).'/includes/Layout/footer.php');?>
 <?php require_once(dirname(__DIR__).'/includes/Layout/scriptsSrc.php');?>
-
+<script src="../js/signupProprietaire.js"></script>
+<script src="../js/validator.js"></script>
 <?php require_once(dirname(__DIR__).'/includes/Layout/finbalise.php');?>
