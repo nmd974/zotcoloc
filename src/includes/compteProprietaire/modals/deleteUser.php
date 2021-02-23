31df29853059ef9f202df26007b3bd7c7bc25a4c
nmd974 <jha.payet@gmail.com>
2021-02-23 14:27:48 +0400

    ajout du code du controller de suppression d'un compte proprietaire

--- src/includes/compteProprietaire/modals/deleteUser.php
+++ src/includes/compteProprietaire/modals/deleteUser.php
@@ -5,16 +5,16 @@
         <h5 class="modal-title" id="deleteUserLabel">Supprimer mon compte</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <form method="post" enctype="multipart/form-data" action="http://127.0.0.1:8000/src/controllers/utilisateurs/particulier/delete.php">
+      <form method="post" enctype="multipart/form-data" action="http://127.0.0.1:8000/src/controllers/utilisateurs/proprietaire/delete.php">
         <div class="modal-body text-center">
-            <div>Etes vous sûr de vouloir supprimer ce contrat ?</div>
+            <div>Etes vous sûr de vouloir supprimer votre compte ?</div>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
           <button type="submit" class="btn btn-success">Supprimer mon compte</button>
         </div>
         <input type="hidden" name="id" value="<?= $_SESSION['id_utilisateur'] ?>">
-        <input type="hidden" name="id_particulier" value="<?= $mon_id_particulier[1][0]->id ?>">
+        <input type="hidden" name="id_proprietaire" value="<?= $mon_compte[1][0]->id ?>">
       </form>
     </div>
   </div>
