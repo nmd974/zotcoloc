0e2004e0c8f9a38bfcffced384b6ee372f5578ed
Drake974 <70998537+Drake974@users.noreply.github.com>
2021-02-25 08:54:17 +0400

    maj

--- src/pages/roommateSearch.php
+++ src/pages/roommateSearch.php
@@ -41,296 +41,31 @@
             <div class="col-md-12 col-lg-4  p-0 mt-1 d-flex flex-wrap text-start">
                 <!-- bouton filtre du loyer -->
                 <!-- barre de recherche -->
-                <!-- <div class="dropdown ms-1 me-1">
-                    <button class="btn btn-light btn-hidden" type="button" id="dropdownMenu2" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        Loyer
-                    </button>
-                    <div class="dropdown-menu p-4 dropdown-rent">
-                        <form action="" method="get">
-                            <p>Budget/mois</p>
-                            <div class="d-flex justify-content-between">
-                                <p class="mb-0"><span id="price-value"></span> €</p>
-                                <p>2000+ €</p>
-                            </div>
-                            <label for="price-range" class="form-label"></label>
-                            <input type="range" class="form-range" min="0" max="2000" step="50" value="0"
-                                id="price-range">
-                            <div class="text-center">
-                                <input class="btn btn-success" type="reset" value="Effacer">
-                                <input class="btn btn-success" type="submit" value="Appliquer">
-                            </div>
-                        </form>
-                    </div>
-                </div> -->
+
+                <?php //require_once(dirname(__DIR__).'/includes/rechercheAnnonce/boutonFiltre/filtrePrix.php');?>
+                
                 <!-- bouton filtre type -->
-                <!-- <div class="dropdown ms-1 me-1">
-                    <button class="btn btn-light btn-hidden" type="button" id="dropdownMenu3" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        Type
-                    </button>
-                    <div class="dropdown-menu p-4 dropdown-type" type="button">
-                        <form action="" method="get">
-                            <p class="h5 mb-4">Type d'annonceur</p>
-                            <select class="form-select mb-4" aria-label="Default select">
-                                <option value="1">Tout</option>
-                                <option value="2">Propriétaire</option>
-                                <option value="3">Résidence</option>
-                                <option value="4">Colocataire</option>
-                                <option value="4">Agence</option>
-                                <option value="4">Coliving</option>
-                            </select>
-                            <div class="text-center">
-                                <input class="btn btn-success" type="reset" value="Effacer">
-                                <input class="btn btn-success" type="submit" value="Appliquer">
-                            </div>
-                        </form>
-                    </div> -->
-                <!-- </div> -->
+
+                <?php //require_once(dirname(__DIR__).'/includes/rechercheAnnonce/boutonFiltre/filtreType.php');?>
+                
+                
                 <!-- bouton filtre disponibilité -->
-                <!-- <div class="dropdown ms-1 me-1">
-                    <button class="btn btn-light btn-hidden" type="button" id="dropdownMenu4" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        Disponibilité
-                    </button>
 
-                    <div class="dropdown-menu p-4 dropdown-availability">
-                        <form action="" method="get">
-                            
-                           <input type="date" name="date" id="" class="form-control mb-4">
-                           
-                            <p class="h5">Durée</p>
-                            <div class="d-flex justify-content-between">
-                                <p class="mb-0"><span id="availability-value"></span> Mois</p>
-                                <p>24+ Mois€</p>
-                            </div>
-                            <label for="availability-range" class="form-label"></label>
-                            <input type="range" class="form-range" min="1" max="24" step="1" value="1"
-                                id="availability-range">
-                            <div class="text-center">
-                                <input class="btn btn-success" type="reset" value="Effacer">
-                                <input class="btn btn-success" type="submit" value="Appliquer">
-                            </div>
-                        </form>
-                    </div>
-                </div> -->
+                <?php //require_once(dirname(__DIR__).'/includes/rechercheAnnonce/boutonFiltre/filtreDisponibilite.php');?>
+
                 <!-- bouton plus de filtre -->
-                 <!-- <div class="dropdown ms-1 me-1"> -->
-                    <!-- <button class="btn btn-light btn-hidden" type="button" id="dropdownMenu5" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        Plus de filtres
-                    </button>
-                    <div class="dropdown-menu p-4 dropdown-filter">
-                        <form action="" method="get">
 
-                            <p class="h5 mb-3">Equipement</p>
-                            <div class="d-flex flex-wrap"> -->
-
-                            <!-- items -->
-                           <!-- <div class="text-center ms-2 me-2">
-                            <div>
-                            <input type="checkbox" class="btn-check" id="btn-check1" autocomplete="off">
-                            <label class="btn btn-outline-success rounded-circle shadow  bouton-check border-light d-flex align-items-center justify-content-center" for="btn-check1"><div><img src="../images/icones/png/9e23c86.png" alt="icon" class="btn-icon"></div></label>
-                            </div> 
-                            <p>salon</p>
-                            </div> -->
-
-                            <!-- items -->
-                            <!-- <div class="text-center ms-2 me-2">
-                            <div>
-                            <input type="checkbox" class="btn-check" id="btn-check2" autocomplete="off">
-                            <label class="btn btn-outline-success rounded-circle shadow  bouton-check border-light d-flex align-items-center justify-content-center" for="btn-check2"><div><img src="../images/icones/png/9e23c86.png" alt="icon" class="btn-icon"></div></label>
-                            </div> 
-                            <p>salon</p>
-                            </div>
-
-                            </div>
-
-                            <hr class="dropdown-divider">
-                            <p class="h5 mb-3">Règles</p>
-
-                            <div class="d-flex flex-wrap"> -->
-
-                            <!-- items -->
-                            <!-- <div class="text-center ms-2 me-2">
-                            <div>
-                            <input type="checkbox" class="btn-check" id="btn-check3" autocomplete="off">
-                            <label class="btn btn-outline-success rounded-circle shadow  bouton-check border-light d-flex align-items-center justify-content-center" for="btn-check3"><div><img src="../images/icones/png/9e23c86.png" alt="icon" class="btn-icon"></div></label>
-                            </div> 
-                            <p>salon</p>
-                            </div> -->
-
-                            <!-- items -->
-                            <!-- <div class="text-center ms-2 me-2">
-                            <div>
-                            <input type="checkbox" class="btn-check" id="btn-check4" autocomplete="off">
-                            <label class="btn btn-outline-success rounded-circle shadow  bouton-check border-light d-flex align-items-center justify-content-center" for="btn-check4"><div><img src="../images/icones/png/9e23c86.png" alt="icon" class="btn-icon"></div></label>
-                            </div> 
-                            <p>salon</p>
-                            </div>
-                            
-                            </div>
-
-                           
-                            <hr class="dropdown-divider">
-                            <div class="d-flex justify-content-between">
-                            <p class="h5">Chambres disponibles</p>
-                            <input type="number" min="1" max="10" value="1" class="form-control number-room">
-                        </div>
-                            <div class="text-center">
-                                <input class="btn btn-success" type="reset" value="Effacer">
-                                <input class="btn btn-success" type="submit" value="Appliquer">
-                            </div>
-                        </form>
-                    </div>
-                </div>  -->
+                <?php //require_once(dirname(__DIR__).'/includes/rechercheAnnonce/boutonFiltre/filtrePlus.php');?>
                 
-                    <!-- bouton modal loyer  version mobile -->
+                <!-- bouton modal loyer  version mobile -->
                 <!-- Button trigger modal -->
-                <!-- <button class="btn btn-light modal-btn ms-1 me-1" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    Loyer
-                </button> -->
-                <!-- bouton modal plus de filtre  version mobile -->
-                <!-- <button class="btn btn-light  modal-btn ms-1 me-1" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal2">
-                    Plus de filtres
-                </button> -->
+
+                <?php //require_once(dirname(__DIR__).'/includes/rechercheAnnonce/modals/loyerMobile.php');?>
                 
                 
-                <!-- Modal -->
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title fw-bold" id="exampleModalLabel">Loyer</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                    <form action="" method="get">
-                                            <p>Budget/mois</p>
-                                            <div class="d-flex justify-content-between">
-                                                <p class="mb-0"><span id="price-value2"></span> €</p>
-                                                <p>2000+ €</p>
-                                            </div>
-                                            <label for="price-range2" class="form-label"></label>
-                                            <input type="range" class="form-range" min="0" max="2000" step="50" value="0"
-                                                id="price-range2">           
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                    </form>
-                    </div>
-                </div>
-                </div>
-
-                    <!-- Modal -->
-                    <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title fw-bold" id="exampleModalLabel2">Plus de filtres</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <!-- disponibilité -->
-                        <div class="modal-body">
-                        <form action="" method="get"> 
-                           <input type="date" name="date" id="" class="form-control mb-4">
-                            <p class="h5">Durée</p>
-                            <div class="d-flex justify-content-between">
-                                <p class="mb-0"><span id="availability-value2"></span> Mois</p>
-                                <p>24+ Mois€</p>
-                            </div>
-                            <label for="availability-range2" class="form-label"></label>
-                            <input type="range" class="form-range" min="1" max="24" step="1" value="1"
-                                id="availability-range2">
-                                <hr>
-                            <!-- type -->
-                            <p class="h5 mb-4">Type d'annonceur</p>
-                            <select class="form-select mb-4" aria-label="Default select">
-                                <option value="1">Tout</option>
-                                <option value="2">Propriétaire</option>
-                                <option value="3">Résidence</option>
-                                <option value="4">Colocataire</option>
-                                <option value="4">Agence</option>
-                                <option value="4">Coliving</option>
-                            </select>
-                        
-                            <hr>
-                            <!-- equipement -->
-                            <p class="h5">Equipement</p>
-
-                            <div class="d-flex flex-wrap">
-
-                            <!-- items -->
-                            <div class="text-center ms-2 me-2">
-                            <div>
-                            <input type="checkbox" class="btn-check" id="btn-check1" autocomplete="off">
-                            <label class="btn btn-outline-success rounded-circle shadow  bouton-check border-light d-flex align-items-center justify-content-center" for="btn-check1"><div><img src="../images/icones/png/9e23c86.png" alt="icon" class="btn-icon"></div></label>
-                            </div> 
-                            <p>salon</p>
-                            </div>
-
-                            <!-- items -->
-                            <div class="text-center ms-2 me-2">
-                            <div>
-                            <input type="checkbox" class="btn-check" id="btn-check2" autocomplete="off">
-                            <label class="btn btn-outline-success rounded-circle shadow  bouton-check border-light d-flex align-items-center justify-content-center" for="btn-check2"><div><img src="../images/icones/png/9e23c86.png" alt="icon" class="btn-icon"></div></label>
-                            </div> 
-                            <p>salon</p>
-                            </div>
-
-                            </div>
-                            
-                            <hr class="dropdown-divider">
-                            <p class="h5">Règles</p>
-
-                            <div class="d-flex flex-wrap">
-
-                            <!-- items -->
-                            <div class="text-center ms-2 me-2">
-                            <div>
-                            <input type="checkbox" class="btn-check" id="btn-check3" autocomplete="off">
-                            <label class="btn btn-outline-success rounded-circle shadow  bouton-check border-light d-flex align-items-center justify-content-center" for="btn-check3"><div><img src="../images/icones/png/9e23c86.png" alt="icon" class="btn-icon"></div></label>
-                            </div> 
-                            <p>salon</p>
-                            </div>
-
-                            <!-- items -->
-                            <div class="text-center ms-2 me-2">
-                            <div>
-                            <input type="checkbox" class="btn-check" id="btn-check4" autocomplete="off">
-                            <label class="btn btn-outline-success rounded-circle shadow  bouton-check border-light d-flex align-items-center justify-content-center" for="btn-check4"><div><img src="../images/icones/png/9e23c86.png" alt="icon" class="btn-icon"></div></label>
-                            </div> 
-                            <p>salon</p>
-                            </div>
-
-                            </div>
-
-                            <hr class="dropdown-divider">
-                            <div class="d-flex justify-content-between">
-                            <p class="h5">Chambres disponibles</p>
-                            <input type="number" min="1" max="10" value="1" class="form-control number-room">
-
-                        
-                    </div>
-
                 
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                    </form>
-                    </div>
-                </div>
-                </div>
 
-            </div>
-
-        </div>
-
-
+            </div>                              
         <div class="row bg-height">
         <!-- carte en commentaire pour la prochaine version -->
             <!-- la map -->
@@ -365,7 +100,7 @@
                 <?php endif;?>
                 </div>
                 <div class="offset-2 col-8 d-flex justify-content-center flex-wrap view-details">
-                    <!-- ////////////////////////carte d'une annonce///////////////////// -->
+                    <!-- ////////////////////////affichage des annonces à partir d'une recherche///////////////////// -->
               <?php if(isset($_GET["btn-search"])):?>  
                
             <?php if(!$annonces[0]):?>
@@ -440,7 +175,7 @@
                     <?php endif; ?>
                     <?php endif; ?>
                         <!-- fin d'une carte -->
-                            <!-- ////////////////////////carte d'une annonce///////////////////// -->
+                            <!-- ////////////////////////affichage des annonces publiés///////////////////// -->
               <?php if(!isset($_GET["btn-search"])):?>  
                 <?php $annonces = Recherches::all_annonce(); ?>
              <?php if(!$annonces[0]):?>
