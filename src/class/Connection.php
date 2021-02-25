477fc761b9f0b9ed2029ebf43ec3529ff70b2eb0
nmd974 <jha.payet@gmail.com>
2021-02-25 10:19:18 +0400

    Ajout de la version amelioree

--- src/class/Connection.php
+++ src/class/Connection.php
@@ -8,6 +8,14 @@
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     }
+
+    public static function getPDOheroku (): PDO
+    {
+        $pdo = new PDO('mysql://eu-cdbr-west-03.cleardb.net;dbname=heroku_ee7443c95f16ffc;charset=utf8', 'bef42a8bdfe427', 'ac306c89');
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }
     
 
  }
--- src/controllers/admin/fill_tables.php
+++ src/controllers/admin/fill_tables.php
@@ -1,2 +1,18 @@
 <?php
 
+require_once(dirname(dirname(__DIR__)).'/class/Connection.php');
+$db = Connection::getPDOheroku();
+
+if($db){
+    try{
+        $query = $db->query('CREATE TABLE `equipements` (
+            `id` varchar(32) NOT NULL,
+            `libelle_equipement` varchar(50) NOT NULL
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4');
+          $db->exec();
+            var_dump("Ajout ok");
+    }catch(PDOException $e){
+        $error = $e->getMessage();
+        var_dump($error);
+    }
+}
