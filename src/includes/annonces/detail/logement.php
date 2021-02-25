259b6b42df5d96e38a3efe27d5d673bc43f6bfaa
Drake974 <70998537+Drake974@users.noreply.github.com>
2021-02-25 10:21:44 +0400

    nettoyage de la page recherche

---
+++ src/includes/annonces/detail/logement.php
@@ -0,0 +1,157 @@
+                <!-- titre logement-->
+                <div class="mb-5 mt-4">
+                    <div class="border-one ps-1">
+                        <div class="border-two ps-3">
+                            <p class="text-secondary m-0 poppins h5">DETAILS</p>
+                            <h2 class="vidaloka m-0 h1">Détails<span class="text-green"> du logement</span></h2>
+                        </div>
+                    </div>
+                </div>
+
+                <p class="h3 mb-3"><?= htmlEntities($annonces[1]->titre_logement) ?></p>
+                <div class="d-flex ">
+                <!-- <p class="h6"> <span class="fw-bold text-danger">5</span>- colocataires</p> -->
+                <p class="h6 me-5"> Logement - <span class="fw-bold"><?= htmlEntities($annonces[1]->surface_logement)?>m<sup>2</sup></span></p>
+                
+                <p class="h6 "> Catégorie - <span class="fw-bold"><?= htmlEntities($annonces[1]->type_logement)?></span></p>
+                </div>
+                <p class="h6 mt-3"><?= htmlEntities($annonces[1]->description_logement) ?></p>
+
+                <!-- titre -->
+                <div class="mb-5 mt-4">
+                    <div class="border-one ps-1">
+                        <div class="border-two ps-3">
+                            <p class="text-secondary m-0 poppins h5">EQUIPEMENTS</p>
+                            <h2 class="vidaloka m-0 h1">Equipements<span class="text-green"> du logement</span></h2>
+                        </div>
+                    </div>
+                </div>
+                <h4 class="text-green h4 mb-4 mt-3">Règles</h4>
+                
+                <?php $regles = Recherches::regleByRoomId(htmlEntities($annonces[1]->id_logement))?>
+                                <?php if(!$regles[0]):?>
+                                <div class="alert alert-danger">Erreur serveur : Impossible de charger le contenu !</div>
+                                <?php else :?>
+                                    <div class="d-flex flex-wrap">
+                                    <?php foreach($regles[1] as $regle):?>
+                                    
+                                      <p class="me-2"><span class="text-green">#</span><?= htmlEntities($regle->libelle_regle)?></p>
+                                    
+                                    <?php endforeach; ?>
+                                    </div>
+                                    <?php endif; ?>
+
+                <hr>
+                <!-- ------------------------------------------------------- -->
+                <h4 class="text-green h4 mb-4 mt-3">Logement</h4>
+                <?php $equipements = Recherches::equipementLogement(htmlEntities($annonces[1]->id_logement))?>
+                                <?php if(!$equipements[0]):?>
+                                <div class="alert alert-danger">Erreur serveur : Impossible de charger le contenu !</div>
+                                <?php else :?>
+                                    <div class="d-flex flex-wrap">
+                                    <?php foreach($equipements[1] as $equipement):?>
+                                    
+                                      <p class="me-2"><span class="text-green">#</span><?= htmlEntities($equipement->libelle_equipement)?></p>
+                                    
+                                    <?php endforeach; ?>
+                                    </div>
+                                    <?php endif;?>
+
+                <!-- <div class="d-flex">
+                
+                <div class="equipment-room border-secondary border shadow mb-2 d-flex align-items-center justify-content-center m-1">
+                    <div class="d-flex">
+                        <div class="d-flex align-items-center">
+                        <i class="fa fa-file-image-o fa-3x text-dark ms-3" aria-hidden="true"></i>
+                        
+                        </div>
+                        <div class="d-flex align-items-center ms-2">
+                            <p class="mb-0">Equipement du logement</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="equipment-room border-secondary border shadow mb-2 d-flex align-items-center justify-content-center m-1">
+                    <div class="d-flex">
+                        <div class="d-flex align-items-center">
+                        <i class="fa fa-file-image-o fa-3x text-dark ms-3" aria-hidden="true"></i>
+                        
+                        </div>
+                        <div class="d-flex align-items-center ms-2">
+                            <p class="mb-0">Equipement du logement</p>
+                        </div>
+                    </div>
+                </div>
+                </div>  -->
+                <hr>
+                <!-- ---------------------------------------------------------- -->
+                <!-- section colocataire du logement -->
+                <!-- <h4 class="text-green h4 mb-4 mt-3">Colocataires</h4>
+                <div class="d-flex justify-content-center flex-wrap">
+                <div class="d-flex flex-column align-items-center ms-4 me-4">
+                <p class="mb-0">Chambre 1</p>
+                <div class="bg-light shadow" style="width:60px; height: 60px; border-radius: 50%;">
+                    <img src="../images/profile.jpg" class="rounded-circle shadow-sm p-1" alt="avatar" style="width:60px;height: 60px; border-radius: 50%;">
+                </div>
+                <p class="text-uppercase mb-0">isabelle</p>
+                <p class="text-green">21 ans</p>
+                </div>
+
+                <div class="d-flex flex-column align-items-center ms-4 me-4">
+                <p class="mb-0">Chambre 2</p>
+                <div class="bg-light shadow" style="width:60px; height: 60px; border-radius: 50%;">
+                    <img src="../images/profile.jpg" class="rounded-circle shadow-sm p-1" alt="avatar" style="width:60px;height: 60px; border-radius: 50%;">
+                </div>
+                <p class="text-uppercase mb-0">isabelle</p>
+                <p class="text-green">21 ans</p>
+                </div>
+
+                <div class="d-flex flex-column align-items-center ms-4 me-4">
+                <p class="mb-0">Chambre 3</p>
+                <div class="bg-light shadow" style="width:60px; height: 60px; border-radius: 50%;">
+                    <img src="../images/profile.jpg" class="rounded-circle shadow-sm p-1" alt="avatar" style="width:60px;height: 60px; border-radius: 50%;">
+                </div>
+                <p class="text-uppercase mb-0"></p>
+                <p class="text-green">Disponible</p>
+                </div>
+
+                <div class="d-flex flex-column align-items-center ms-4 me-4">
+                <p class="mb-0">Chambre 4</p>
+                <div class="bg-light shadow" style="width:60px; height: 60px; border-radius: 50%;">
+                    <img src="../images/profile.jpg" class="rounded-circle shadow-sm p-1" alt="avatar" style="width:60px;height: 60px; border-radius: 50%;">
+                </div>
+                <p class="text-uppercase mb-0"></p>
+                <p class="text-green">Disponible</p>
+                <button type="button" class="btn btn-outline-secondary btn-sm">Visiter</button>
+                </div>
+                
+                </div>
+                <hr> -->
+                <!-- <h4 class="text-green h4 mb-4 mt-3">Quartier</h4> -->
+                <!-- map -->
+                <!-- <div id="mapdetails">
+                </div>
+                <hr> -->
+                <!-- -------------------------------------------------------------- -->
+                <h4 class="text-green h4 mb-4 mt-3">Explorer ce logement</h4>
+                <?php $photo_logements = Recherches::photoLogementById(htmlEntities($annonces[1]->id_logement))?>
+                
+                                <?php if(!$photo_logements[0]):?>
+                                <div class="alert alert-danger">Erreur serveur : Impossible de charger le contenu !</div>
+                                <?php else :?>
+                                    <?php foreach($photo_logements[1] as $photo_logement):?>
+                                    <div>
+                                    
+                                <img src="../images/<?= htmlEntities($photo_logement->libelle_photo) ?>" class="" alt="" style="width:60px; height: 60px;">
+                                </div>
+                                <?php endforeach; ?>
+                                <?php endif; ?>
+                <div class="img-wrapper mb-4">
+                <div class="img-one"><img src="../images/bright-hotel-room-bed.jpg" alt="image" class="img-fluid"></div>
+                <div class="img-two"><img src="../images/bright-hotel-room-bed.jpg" alt="image" class="img-fluid"></div>
+                <div class="img-three"><img src="../images/bright-hotel-room-bed.jpg" alt="image" class="img-fluid"></div>
+                <div class="img-four"><img src="../images/bright-hotel-room-bed.jpg" alt="image" class="img-fluid"></div>
+                <div class="img-five"><img src="../images/bright-hotel-room-bed.jpg" alt="image" class="img-fluid"></div>
+                <div class="img-six"><img src="../images/bright-hotel-room-bed.jpg" alt="image" class="img-fluid"></div>
+                <div class="img-six"><img src="../images/bright-hotel-room-bed.jpg" alt="image" class="img-fluid"></div>
+                <div class="img-six"><img src="../images/bright-hotel-room-bed.jpg" alt="image" class="img-fluid"></div>
--- src/pages/roommateSearch.php
+++ src/pages/roommateSearch.php
@@ -11,59 +11,31 @@
 <section class="container-fluid margin-search">
         <div class="row">
             <div class="col-lg-8 col-md-12">
-                <!-- barre de recherche -->
-                <div class="input-border bg-light mb-3 input-filter">
-                    <form method="get" class="form-group">
-                        <div class="input-group">
-                            <input type="text" name="search-room" class="form-control location-border2" placeholder="Lieux"
-                                aria-label="location" aria-describedby="button-addon1" id="search" list="datalistOptions" autocomplete="off">
-                                <datalist id="datalistOptions"> 
-                                
-                                <?php $liste_villes = Recherches::listVille()?>
-                                <?php if(!$liste_villes[0]):?>
-                                <div class="alert alert-danger">Erreur serveur : Impossible de charger le contenu !</div>
-                                <?php else :?>
-                                    <?php foreach($liste_villes[1] as $liste_ville):?>
-                                        <option value="<?=$liste_ville->libelle_ville?>">
-                                <?php endforeach; ?>
-                                <?php endif; ?>
-                                </datalist>
-                                
-                               
-                                <div class="result" id="result-search"></div>
-                                
-                            <button class="btn btn-outline-secondary w-25 bg-green text-white btn-radius ms-1" type="submit"
-                                id="button-addon2" name="btn-search">Recherche</button>
-                        </div>
-                    </form>
-                </div>
+            <?php require_once(dirname(__DIR__).'/includes/annonces/recherche/input.php');?>
+                
             </div>
             <div class="col-md-12 col-lg-4  p-0 mt-1 d-flex flex-wrap text-start">
                 <!-- bouton filtre du loyer -->
                 <!-- barre de recherche -->
 
-                <?php //require_once(dirname(__DIR__).'/includes/rechercheAnnonce/boutonFiltre/filtrePrix.php');?>
+                <?php //require_once(dirname(__DIR__).'/includes/annonces/recherche/filtre/prix.php');?>
                 
                 <!-- bouton filtre type -->
 
-                <?php //require_once(dirname(__DIR__).'/includes/rechercheAnnonce/boutonFiltre/filtreType.php');?>
-                
+                <?php //require_once(dirname(__DIR__).'/includes/annonces/recherche/filtre/type.php');?>
                 
                 <!-- bouton filtre disponibilité -->
 
-                <?php //require_once(dirname(__DIR__).'/includes/rechercheAnnonce/boutonFiltre/filtreDisponibilite.php');?>
+                <?php //require_once(dirname(__DIR__).'/includes/annonces/recherche/filtre/disponibilite.php');?>
 
                 <!-- bouton plus de filtre -->
 
-                <?php //require_once(dirname(__DIR__).'/includes/rechercheAnnonce/boutonFiltre/filtrePlus.php');?>
+                <?php //require_once(dirname(__DIR__).'/includes/annonces/recherche/filtre/plus.php');?>
                 
                 <!-- bouton modal loyer  version mobile -->
                 <!-- Button trigger modal -->
 
-                <?php //require_once(dirname(__DIR__).'/includes/rechercheAnnonce/modals/loyerMobile.php');?>
-                
-                
-                
+                <?php //require_once(dirname(__DIR__).'/includes/annonces/recherche/modals/loyerMobile.php');?>
 
             </div>                              
         <div class="row bg-height">
@@ -73,183 +45,15 @@
                 <div id="map"></div>
             </div> -->
             <div class="col-lg-12 col-md-12 card-order mt-3">
-                <!-- titre -->
-                <?php if(isset($_GET["btn-search"])):?>
-                <div class="mb-5">
-                    <div class="border-one ps-1">
-                        <div class="border-two ps-3">
-                            <p class="text-secondary m-0 poppins h5">RESULTATS</p>
-                            <h2 class="vidaloka m-0 h2"><?php echo htmlEntities($nombres[1][0]["COUNT(*)"]);?><span class="text-green"> résultats</span> pour votre recherche
-                            </h2>
-                        </div>
-                    </div>
-                </div>
-                <?php endif;?>
-                <?php if(!isset($_GET["btn-search"])):?>
-                    <?php $count = Recherches::count_annonce(); ?>
-                   
-                <div class="mb-5">
-                    <div class="border-one ps-1">
-                        <div class="border-two ps-3">
-                            <p class="text-secondary m-0 poppins h5">ANNONCES</p>
-                            <h2 class="vidaloka m-0 h2"><?php echo htmlEntities($count[1][0]["COUNT(*)"]);?><span class="text-green"> annonces</span> actuellement
-                            </h2>
-                        </div>
-                    </div>
-                </div>
-                <?php endif;?>
-                </div>
-                <div class="offset-2 col-8 d-flex justify-content-center flex-wrap view-details">
+                <?php require_once(dirname(__DIR__).'/includes/annonces/recherche/titre.php');?>
+            </div>
+            <div class="offset-2 col-8 d-flex justify-content-center flex-wrap view-details">
                     <!-- ////////////////////////affichage des annonces à partir d'une recherche///////////////////// -->
-              <?php if(isset($_GET["btn-search"])):?>  
-               
-            <?php if(!$annonces[0]):?>
-            <div class="alert alert-danger">Erreur serveur : Impossible de charger le contenu !</div>
-            <?php else :?>
-                <?php foreach($annonces[1] as $annonce):?>
-                
-                    <div class="m-2 mb-4">
-                    <a href="roommateDetails.php?id=<?= htmlEntities($annonce->id_chambre) ?>" class="mode-link">
-                        <div class="card card-relative shadow-lg border " style="width: 18rem;">
-                            <!-- icon coeur en position absolute-->
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                    class="bi bi-heart heart" viewBox="0 0 16 16">
-                                    <path
-                                        d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                                </svg>
-                            </div>
-                            <!-- avatar en position absolute -->
-                            <div class="avatar d-flex justify-content-center align-items-center bg-light shadow" style="width:60px; height: 60px; border-radius: 50%;">
-                            <?php $utilisateurs = Recherches::photo_utilisateur(htmlEntities($annonce->id_utilisateur))?>
-                                <?php if(!$utilisateurs[0]):?>
-                                <div class="alert alert-danger">Erreur serveur : Impossible de charger le contenu !</div>
-                                <?php else :?>
-                                    <?php foreach($utilisateurs[1] as $utilisateur):?>
-                                <img src="../images/<?= htmlEntities($utilisateur->libelle_photo) ?>" class="rounded-circle shadow-sm p-1" alt="avatar" style="width:60px; height: 60px; border-radius: 50%;">
-                                <?php endforeach; ?>
-                                    <?php endif; ?>
-                            </div>
-                            <!-- caroussel -->
-                            <div id="carouselExampleIndicators" class="carousel slide " data-bs-ride="carousel">
-                                <ol class="carousel-indicators">
-                                
-                                    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active">
-                                    </li>
-                                    
-                                    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
-                                    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
-                                </ol>
-                                <div class="carousel-inner" >
-
-                               <?php $images = Recherches::photo_annonce(htmlEntities($annonce->id_chambre))?>
-                                <?php if(!$images[0]):?>
-                                <div class="alert alert-danger">Erreur serveur : Impossible de charger le contenu !</div>
-                                <?php else :?>
-                                    <?php foreach($images[1] as $image):?>
-                                    <div class="carousel-item active">
-                                        <img src="../images/<?= htmlEntities($image->libelle_photo) ?>" class="d-block w-100 " alt="room" style="height:190px;">
-                                    </div>
-                                    <?php endforeach; ?>
-                                    <?php endif; ?>
-                                  
-                                </div>
-                            </div>
-                            <!-- descriptif de l'annonce -->
-                            <div class="card-body">
-                            
-                            <!-- role -->
-                                <span class="badge bg-primary mb-1 letter-space"><?= htmlEntities($annonce->libelle_role) ?></span>
-                                <!-- titre de l'annonce -->
-                                <h5 class="card-title"><?= htmlEntities($annonce->titre_chambre) ?></h5>
-                                <p class="card-text"><?= htmlEntities($annonce->libelle_ville) ?></p>
-                                <!-- <p class="card-text">Chambre: 1</p> -->
-                                <p class="card-text"><?= htmlEntities($annonce->date_disponibilite) ?></p>
-                                <p class="card-text"><span class="fw-bold h4"><?= htmlEntities($annonce->loyer) ?> €</span> par mois</p>
-                                
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <?php endforeach; ?>
-                    <?php endif; ?>
-                    <?php endif; ?>
-                        <!-- fin d'une carte -->
+                <?php require_once(dirname(__DIR__).'/includes/annonces/recherche/resultat.php');?>
+              
                             <!-- ////////////////////////affichage des annonces publiés///////////////////// -->
-              <?php if(!isset($_GET["btn-search"])):?>  
-                <?php $annonces = Recherches::all_annonce(); ?>
-             <?php if(!$annonces[0]):?>
-             <div class="alert alert-danger">Erreur serveur : Impossible de charger le contenu !</div>
-             <?php else :?>
-                 <?php foreach($annonces[1] as $annonce):?>
-                 
-                     <div class="m-2 mb-4">
-                     <a href="roommateDetails.php?id=<?= htmlEntities($annonce->id_chambre) ?>" class="mode-link">
-                         <div class="card card-relative shadow-lg border " style="width: 18rem;">
-                             <!-- icon coeur en position absolute-->
-                             <div>
-                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                                     class="bi bi-heart heart" viewBox="0 0 16 16">
-                                     <path
-                                         d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                                 </svg>
-                             </div>
-                             <!-- avatar en position absolute -->
-                             <div class="avatar d-flex justify-content-center align-items-center bg-light shadow" style="width:60px; height: 60px; border-radius: 50%;">
-                             <?php $utilisateurs = Recherches::photo_utilisateur(htmlEntities($annonce->id_utilisateur))?>
-                                 <?php if(!$utilisateurs[0]):?>
-                                 <div class="alert alert-danger">Erreur serveur : Impossible de charger le contenu !</div>
-                                 <?php else :?>
-                                     <?php foreach($utilisateurs[1] as $utilisateur):?>
-                                 <img src="../images/<?= htmlEntities($utilisateur->libelle_photo) ?>" class="rounded-circle shadow-sm p-1" alt="avatar" style="width:60px; height: 60px; border-radius: 50%;">
-                                 <?php endforeach; ?>
-                                     <?php endif; ?>
-                             </div>
-                             <!-- caroussel -->
-                             <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                                 <ol class="carousel-indicators">
-                                 
-                                     <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active">
-                                     </li>
-                                     
-                                     <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
-                                     <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
-                                 </ol>
-                                 <div class="carousel-inner">
- 
-                                <?php $images = Recherches::photo_annonce(htmlEntities($annonce->id_chambre))?>
-                                 <?php if(!$images[0]):?>
-                                 <div class="alert alert-danger">Erreur serveur : Impossible de charger le contenu !</div>
-                                 <?php else :?>
-                                     <?php foreach($images[1] as $image):?>
-                                     <div class="carousel-item active">
-                                         <img src="../images/<?= htmlEntities($image->libelle_photo) ?>" class="d-block w-100" alt="room" style="height:190px;">
-                                     </div>
-                                     <?php endforeach; ?>
-                                     <?php endif; ?>
-                                   
-                                 </div>
-                             </div>
-                             <!-- descriptif de l'annonce -->
-                             <div class="card-body">
-                             
-                             <!-- role -->
-                                 <span class="badge bg-primary mb-1 letter-space"><?= htmlEntities($annonce->libelle_role) ?></span>
-                                 <!-- titre de l'annonce -->
-                                 <h5 class="card-title"><?= htmlEntities($annonce->titre_chambre) ?></h5>
-                                 <p class="card-text"><?= htmlEntities($annonce->libelle_ville) ?></p>
-                                 <!-- <p class="card-text">Chambre: 1</p> -->
-                                 <p class="card-text"><?= htmlEntities($annonce->date_disponibilite) ?></p>
-                                 <p class="card-text"><span class="fw-bold h4"><?= htmlEntities($annonce->loyer) ?> €</span> par mois</p>
-                                
-                                 </a>
-                             </div>
-                         </div>
-                     </div>
-                     <?php endforeach; ?>
-                     <?php endif; ?>
-                     <?php endif; ?>
-                         <!-- fin d'une carte -->
+                <?php require_once(dirname(__DIR__).'/includes/annonces/recherche/resultatAll.php');?>
+              
          
                 </div>
             </div>
