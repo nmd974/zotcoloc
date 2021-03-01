fe78f3f48fd33dad6bdd90a216e91cb157943632
nmd974 <jha.payet@gmail.com>
2021-03-01 12:38:13 +0400

    ajout de la modification du style signup login

--- src/pages/authentificationLoueur.php
+++ src/pages/authentificationLoueur.php
@@ -11,16 +11,16 @@
         </div>
     </div>
 </div>
-<div class="container d-flex mb-5" id="wrapper_page_content">
+<div class="container d-flex" id="wrapper_page_content">
 
 <div class="form-modal">
-<div id="signup-form" class="mb-5 mt-5">
-
+<div id="signup-form" class="mb-5">
+<a href="./seconnecter.php" class="mt-4 mx-3">J'ai déjà un compte</a>
 <?php if(isset($_SESSION['flash'])):?>
     <div class="alert alert-danger mb-2"><?=  $_SESSION['flash'][1] ?></div>
 <?php endif;?>
 
-<form method="POST" enctype="multipart/form-data" class="mt-5" id="proprietaire_inscription" action="<?php getenv("URL_APP")?>/src/controllers/utilisateurs/proprietaire/create.php">
+<form method="POST" enctype="multipart/form-data" id="proprietaire_inscription" action="<?php getenv("URL_APP")?>/src/controllers/utilisateurs/proprietaire/create.php">
     <!--Nom-->
     <div class="col-md-12 mb-3">
         <label for="nom_proprietaire" class="form-label">Nom*</label><br>
@@ -71,10 +71,7 @@
 </form>
 
 </div>
-<div class="text-center d-flex flex-column justify-content-center align-items-center mb-5">
-        Vous avez déjà un compte ? 
-        <a href="./seconnecter.php">Se connecter</a>
-    </div>
+
 </div>
 <style>
 .form-modal{
