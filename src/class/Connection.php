a16e121ede6a661e5f61019767af2146f5bc33e8
nmd974 <jha.payet@gmail.com>
2021-03-02 15:30:23 +0400

    ajout du charset pour connexion bdd

--- src/class/Connection.php
+++ src/class/Connection.php
@@ -17,7 +17,8 @@
         $active_group = 'default';
         $query_builder = TRUE;
         // Connect to DB
-        $pdo = new PDO("mysql:host=".$cleardb_server."; dbname=".$cleardb_db, $cleardb_username, $cleardb_password, "charset=utf8");
+        $pdo = new PDO("mysql:host=".$cleardb_server."; dbname=".$cleardb_db, $cleardb_username, $cleardb_password);
+        $pdo->exec('SET NAMES utf8');
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
