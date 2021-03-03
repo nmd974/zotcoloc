632c426ed920907cddd6bd37b040ae423566a7ed
nmd974 <jha.payet@gmail.com>
2021-03-03 21:26:56 +0400

    suppression fichiers inutiles + arrangement du login et signup

--- src/pages/admin.php
+++ src/pages/admin.php
@@ -1,8 +1,8 @@
 <?php require_once(dirname(__DIR__).'/includes/Layout/header.php');?>
 <?php
-    if(!$_SESSION['isLoggedIn'] || $_SESSION['role'] != "administrateur"){
-        header('Location: home.php');
-    }
+if(!$_SESSION['isLoggedIn'] || $_SESSION['role'] != "administrateur"){
+    header('Location: home.php');
+}
 ?>
 <?php require_once(dirname(__DIR__).'/class/Interets.php');?>
 <?php require_once(dirname(__DIR__).'/class/Utilisateurs.php');?>
@@ -10,61 +10,57 @@
 <?php require_once(dirname(__DIR__).'/controllers/editProfilParticulier.php');?>
 
 <?php
-        //Chargement des données correspondantes à l'id lors de l'arrivée sur page
-        
-        $mon_id_particulier = Utilisateurs::monCompteParticulier($_SESSION['id_utilisateur']);
-        $ma_photo = Photos::photosByIdUser($_SESSION['id_utilisateur']);
-        $mes_interets = Interets::interetsByParticulierId($mon_id_particulier[1][0]->id);
-        $mes_interet_list = [];
-        foreach($mes_interets[1] as $interet){
-            array_push($mes_interet_list, $interet->id_interet);
-        }
-        $mes_favoris = Utilisateurs::favorisParticulier($mon_id_particulier[1][0]->id);
-        $mes_candidatures = Utilisateurs::candidaturesParticulier($mon_id_particulier[1][0]->id);
-        $mes_annonces = Utilisateurs::annoncesParticulier($_SESSION['id_utilisateur']);
+//Chargement des données correspondantes à l'id lors de l'arrivée sur page
+
+$mon_id_particulier = Utilisateurs::monCompteParticulier($_SESSION['id_utilisateur']);
+$ma_photo = Photos::photosByIdUser($_SESSION['id_utilisateur']);
+$mes_interets = Interets::interetsByParticulierId($mon_id_particulier[1][0]->id);
+$mes_interet_list = [];
+foreach($mes_interets[1] as $interet){
+    array_push($mes_interet_list, $interet->id_interet);
+}
+$mes_favoris = Utilisateurs::favorisParticulier($mon_id_particulier[1][0]->id);
+$mes_candidatures = Utilisateurs::candidaturesParticulier($mon_id_particulier[1][0]->id);
+$mes_annonces = Utilisateurs::annoncesParticulier($_SESSION['id_utilisateur']);
 ?>
 
 <?php if(isset($_POST['save_photo_user'])):?>
-  <?php if(empty($ma_photo[1])):?>
-      <?php $ajout_photo = photoUtilisateur($_FILES['image_upload'], $_SESSION['id_utilisateur'], '');?>
-        <?php $ma_photo = Photos::photosByIdUser($_SESSION['id_utilisateur']);?>
-      <?php else: ?>
-        <?php $ajout_photo = photoUtilisateur($_FILES['image_upload'], $_SESSION['id_utilisateur'], $ma_photo[1][0]->libelle_photo);?>
-        <?php $ma_photo = Photos::photosByIdUser($_SESSION['id_utilisateur']);?>
-      <?php endif; ?>
+<?php if(empty($ma_photo[1])):?>
+<?php $ajout_photo = photoUtilisateur($_FILES['image_upload'], $_SESSION['id_utilisateur'], '');?>
+<?php $ma_photo = Photos::photosByIdUser($_SESSION['id_utilisateur']);?>
+<?php else: ?>
+<?php $ajout_photo = photoUtilisateur($_FILES['image_upload'], $_SESSION['id_utilisateur'], $ma_photo[1][0]->libelle_photo);?>
+<?php $ma_photo = Photos::photosByIdUser($_SESSION['id_utilisateur']);?>
+<?php endif; ?>
 <?php endif; ?>
 
 <?php 
-    if(isset($_POST['save_edit_info_coloc'])){
-        $update = editInfosColoc($_POST, $mon_id_particulier[1][0]->id);
-        unset($_POST);
-        $mon_id_particulier = Utilisateurs::monCompteParticulier($_SESSION['id_utilisateur']);
-    }
+if(isset($_POST['save_edit_info_coloc'])){
+    $update = editInfosColoc($_POST, $mon_id_particulier[1][0]->id);
+    unset($_POST);
+    $mon_id_particulier = Utilisateurs::monCompteParticulier($_SESSION['id_utilisateur']);
+}
 ?>
 
 <?php 
-    if(isset($_POST['save_edit_info_perso'])){
-        $update = editInfosPerso($_POST, $mon_id_particulier[1][0]->id);
-        $mon_id_particulier = Utilisateurs::monCompteParticulier($_SESSION['id_utilisateur']);
-    }
+if(isset($_POST['save_edit_info_perso'])){
+    $update = editInfosPerso($_POST, $mon_id_particulier[1][0]->id);
+    $mon_id_particulier = Utilisateurs::monCompteParticulier($_SESSION['id_utilisateur']);
+}
 ?>
 
 <?php 
-    if(isset($_POST['save_edit_interets'])){
-        $update = editInterets($_POST, $mon_id_particulier[1][0]->id, $mes_interet_list);
-        $mes_interets = Interets::interetsByParticulierId($mon_id_particulier[1][0]->id);
-        $mes_interet_list = [];
-        foreach($mes_interets[1] as $interet){
-            array_push($mes_interet_list, $interet->id_interet);
-        }
+if(isset($_POST['save_edit_interets'])){
+    $update = editInterets($_POST, $mon_id_particulier[1][0]->id, $mes_interet_list);
+    $mes_interets = Interets::interetsByParticulierId($mon_id_particulier[1][0]->id);
+    $mes_interet_list = [];
+    foreach($mes_interets[1] as $interet){
+        array_push($mes_interet_list, $interet->id_interet);
     }
+}
 ?>
 
 <?php require_once(dirname(__DIR__).'/includes/compteParticulier/modals/photoUser.php');?>
-<?php require_once(dirname(__DIR__).'/includes/compteParticulier/modals/infosColoc.php');?>
-<?php require_once(dirname(__DIR__).'/includes/compteParticulier/modals/infosPerso.php');?>
-<?php require_once(dirname(__DIR__).'/includes/compteParticulier/modals/interets.php');?>
-<?php require_once(dirname(__DIR__).'/includes/compteParticulier/modals/deleteUser.php');?>
 <?php require_once(dirname(__DIR__).'/includes/compteParticulier/sidebar.php');?>
 
 <div class="container-fluid" id="wrapper-content">
@@ -87,13 +83,13 @@
         <?php require_once(dirname(__DIR__).'/includes/compteParticulier/dashboard.php');?>
         <?php require_once(dirname(__DIR__).'/includes/compteParticulier/favoris.php');?>
     </div>
-
+    
     <!--BOUTON DE SWITCH SIDEBAR EN MOBILE-->
     <i class="fa fa-user fa-2x" id="menu-toggle" aria-hidden="true"></i>
 </div>
 
 <?php //require_once(dirname(__DIR__).'/includes/Layout/footer.php');?>
 <?php require_once(dirname(__DIR__).'/includes/Layout/scriptsSrc.php');?>
-<script src="../js/sidebar.js"></script>
+<script src="../js/sidebar_admin.js"></script>
 <script src="../js/modals.js"></script>
 <?php require_once(dirname(__DIR__).'/includes/Layout/finbalise.php');?>
--- src/pages/authentificationLoueur.php
+++ src/pages/authentificationLoueur.php
@@ -12,258 +12,58 @@
     </div>
 </div>
 <div class="container d-flex" id="wrapper_page_content">
-
-<div class="form-modal">
-<div id="signup-form" class="mb-5">
-<a href="./seconnecter.php" class="mt-4 mx-3">J'ai déjà un compte</a>
-<?php if(isset($_SESSION['flash'])):?>
-    <div class="alert alert-danger mb-2"><?=  $_SESSION['flash'][1] ?></div>
-<?php endif;?>
-
-<form method="POST" enctype="multipart/form-data" id="proprietaire_inscription" action="<?php getenv("URL_APP")?>/src/controllers/utilisateurs/proprietaire/create.php">
-    <!--Nom-->
-    <div class="col-md-12 mb-3">
-        <label for="nom_proprietaire" class="form-label">Nom*</label><br>
-        <input type="text" name="nom_proprietaire" class="form-control" id="nom_proprietaire" value="<?php if(isset($_POST['nom_proprietaire'])){
-            echo $_POST['nom_proprietaire'];
-        }?>">
-        
+    <div class="form-modal">
+        <div id="signup-form" class="mb-5">
+            <?php if(isset($_SESSION['flash'])):?>
+            <div class="alert alert-danger mb-2"><?=  $_SESSION['flash'][1] ?></div>
+            <?php endif;?>
+            <form method="POST" enctype="multipart/form-data" id="proprietaire_inscription" action="<?php getenv("URL_APP")?>/src/controllers/utilisateurs/proprietaire/create.php">
+                <!--Nom-->
+                <div class="col-md-12 mb-3">
+                    <label for="nom_proprietaire" class="form-label">Nom*</label><br>
+                    <input type="text" name="nom_proprietaire" class="form-control" id="nom_proprietaire" value="<?= $_POST['nom_proprietaire']?: htmlspecialchars($_POST['nom_proprietaire'], ENT_QUOTES);?>">
+                </div>
+                <!--Prenom-->
+                <div class="col-md-12 mb-3">
+                    <label for="prenom_proprietaire" class="form-label">Prénom*</label><br>
+                    <input type="text" name="prenom_proprietaire" class="form-control" id="prenom_proprietaire" value="<?= $_POST['prenom_proprietaire']?: htmlspecialchars($_POST['prenom_proprietaire'], ENT_QUOTES);?>">
+                </div>
+                <!--téléphone-->
+                <div class="col-md-12 mb-3">
+                    <label for="telephone" class="form-label">Téléphone*</label>
+                    <input type="tel" class="form-control" id="telephone" name="telephone" value="<?= $_POST['telephone']?: htmlspecialchars($_POST['telephone'], ENT_QUOTES);?>">
+                </div>
+                <!--Email-->
+                <div class="col-md-12 mb-3">
+                    <label for="email" class="form-label">Email*</label>
+                    <input type="email" class="form-control" id="email" name="email" value="<?= $_POST['email']?: htmlspecialchars($_POST['email'], ENT_QUOTES);?>">
+                    <em id="valid_email"></em>
+                </div>
+                <!--mot passe-->
+                <div class="col-md-12 mb-3">
+                    <label for="password" class="form-label">Mot de passe*</label>
+                    <input type="password" class="form-control" id="password" name="password">
+                </div>
+                <!--confirmer mot passe-->
+                <div class="col-md-12 mb-3">
+                    <label for="confirm_password" class="form-label">Confirmer le mot de passe*</label>
+                    <input type="password" class="form-control" id="confirm_password" name="confirm_password">
+                </div>
+                <div class="col-md-12 mt-4">
+                    *Champs obligatoires
+                </div>
+                <!--button validation inscription-->
+                <div class="col-12 text-end my-4">
+                    <button type="submit" class="btn w-55 bg-green text-white mr-5" id="ajouter_proprietaire" name="ajouter_proprietaire">Je m'inscris</button>
+                </div>
+            </form>
+            <div class="mt-5">
+                <p>J'ai déjà mon compte<p>
+                    <a href="./seconnecter.php" class="mt-4 mx-3">Je me connecte</a>
+                </div>
+            </div>
+        </div>
     </div>
-    <!--Prenom-->
-    <div class="col-md-12 mb-3">
-        <label for="prenom_proprietaire" class="form-label">Prénom*</label><br>
-        <input type="text" name="prenom_proprietaire" class="form-control" id="prenom_proprietaire" value="<?php if(isset($_POST['prenom_proprietaire'])){
-            echo $_POST['prenom_proprietaire'];
-        }?>">
-    </div>
-    <!--téléphone-->
-    <div class="col-md-12 mb-3">
-        <label for="telephone" class="form-label">Téléphone*</label>
-        <input type="tel" class="form-control" id="telephone" name="telephone" value="<?php if(isset($_POST['telephone'])){
-            echo $_POST['telephone'];
-        }?>">
-    </div>
-    <!--Email-->
-    <div class="col-md-12 mb-3">
-        <label for="email" class="form-label">Email*</label>
-        <input type="email" class="form-control" id="email" name="email" value="<?php if(isset($_POST['email'])){
-            echo $_POST['email'];
-        }?>">
-        <em id="valid_email"></em>
-    </div>
-    <!--mot passe-->
-    <div class="col-md-12 mb-3">
-        <label for="password" class="form-label">Mot de passe*</label>
-        <input type="password" class="form-control" id="password" name="password">
-    </div>
-    <!--confirmer mot passe-->
-    <div class="col-md-12 mb-3">
-        <label for="confirm_password" class="form-label">Confirmer le mot de passe*</label>
-        <input type="password" class="form-control" id="confirm_password" name="confirm_password">
-    </div>
-    <div class="col-md-12 mt-4">
-        *Champs obligatoires
-    </div>
-    <!--button validation inscription-->
-    <div class="col-12 text-end my-4">
-        <button type="submit" class="btn w-55 bg-green text-white mr-5" id="ajouter_proprietaire" name="ajouter_proprietaire">Je m'inscris</button>
-    </div>
-</form>
-
-</div>
-
-</div>
-<style>
-.form-modal{
-position: relative;
-    width:450px;
-    height:auto;
-    margin-top:4em;
-    left:50%;
-    transform:translateX(-50%);
-    background:#fff;
-    border-top-right-radius: 5px;
-    border-top-left-radius: 5px;
-    border-bottom-right-radius: 5px;
-    box-shadow:0 3px 20px 0px rgba(0, 0, 0, 25%)
-}
-
-.form-modal button{
-    cursor: pointer;
-    position: relative;
-    text-transform: capitalize;
-    font-size:1em;
-    z-index: 2;
-    outline: none;
-    background:#fff;
-    transition:0.2s;
-}
-
-.form-modal .btn{
-    border-radius: 5px;
-    border:none;
-    font-weight: bold;
-    font-size:1.2em;
-    padding:0.8em 1em 0.8em 1em!important;
-    transition:0.5s;
-    border:1px solid #ebebeb;
-    margin-bottom:0.5em;
-    margin-top:0.5em;
-}
-
-.form-modal .login , .form-modal .signup{
-    background:#57b846;
-    color:#fff;
-}
-
-.form-modal .login:hover , .form-modal .signup:hover{
-    background:#222;
-}
-
-.form-toggle{
-    position: relative;
-    width:100%;
-    height:auto;
-}
-
-.form-toggle button{
-    width:50%;
-    float:left;
-    padding:1.5em;
-    margin-bottom:1.5em;
-    border:none;
-    transition: 0.2s;
-    font-size:1.1em;
-    font-weight: bold;
-    border-top-right-radius: 5px;
-    border-top-left-radius: 5px;
-}
-
-.form-toggle button:nth-child(1){
-    border-bottom-right-radius: 5px;
-}
-
-.form-toggle button:nth-child(2){
-    border-bottom-left-radius: 5px;
-}
-
-#login-toggle{
-    background:#57b846;
-    color:#ffff;
-}
-
-.form-modal form{
-    position: relative;
-    width:90%;
-    height:auto;
-    left:50%;
-    transform:translateX(-50%);  
-}
-
-#login-form button , #signup-form button{
-    width:100%;
-    margin-top:0.5em;
-    padding:0.6em;
-}
-
-.form-modal input{
-    position: relative;
-    width:100%;
-    font-size:1em;
-    padding:1.2em 1.7em 1.2em 1.7em;
-    margin-top:0.6em;
-    margin-bottom:0.6em;
-    border-radius: 5px;
-    border:none;
-    background:#ebebeb;
-    outline:none;
-    font-weight: bold;
-    transition:0.4s;
-}
-
-.form-modal input:focus , .form-modal input:active{
-    transform:scaleX(1.02);
-}
-
-.form-modal input::-webkit-input-placeholder{
-    color:#222;
-}
-
-
-.form-modal p{
-    font-size:16px;
-    font-weight: bold;
-}
-
-.form-modal p a{
-    color:#57b846;
-    text-decoration: none;
-    transition:0.2s;
-}
-
-.form-modal p a:hover{
-    color:#222;
-}
-
-
-.form-modal i {
-    position: absolute;
-    left:10%;
-    top:50%;
-    transform:translateX(-10%) translateY(-50%); 
-}
-
-.fa-google{
-    color:#dd4b39;
-}
-
-.fa-linkedin{
-    color:#3b5998;
-}
-
-.fa-windows{
-    color:#0072c6;
-}
-
-.-box-sd-effect:hover{
-    box-shadow: 0 4px 8px hsla(210,2%,84%,.2);
-}
-
-@media only screen and (max-width:500px){
-    .form-modal{
-        width:100%;
-    }
-}
-
-@media only screen and (max-width:400px){
-    i{
-        display: none!important;
-    }
-}
-</style>
-
-<script>
-// function toggleSignup(){
-//    document.getElementById("login-toggle").style.backgroundColor="#fff";
-//     document.getElementById("login-toggle").style.color="#222";
-//     document.getElementById("signup-toggle").style.backgroundColor="#57b846";
-//     document.getElementById("signup-toggle").style.color="#fff";
-//     document.getElementById("login-form").style.display="none";
-//     document.getElementById("signup-form").style.display="block";
-// }
-
-// function toggleLogin(){
-//     document.getElementById("login-toggle").style.backgroundColor="#57B846";
-//     document.getElementById("login-toggle").style.color="#fff";
-//     document.getElementById("signup-toggle").style.backgroundColor="#fff";
-//     document.getElementById("signup-toggle").style.color="#222";
-//     document.getElementById("signup-form").style.display="none";
-//     document.getElementById("login-form").style.display="block";
-// }
-
-</script>
-
-   
 </div>
 
 <?php require_once(dirname(__DIR__).'/includes/Layout/footer.php');?>
