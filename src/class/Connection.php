db0fff0bda08dec41dc083b0947d8d4dcdb9aae3
Drake974 <70998537+Drake974@users.noreply.github.com>
2021-03-04 18:51:38 +0400

    ajout de chambre test

--- src/class/Connection.php
+++ src/class/Connection.php
@@ -6,23 +6,23 @@
     public static function getPDO (): PDO
     {
         // Code a activer en local
-        // $pdo = new PDO('mysql:host=127.0.0.1;dbname=zotcoloc;charset=utf8', 'root', '');
-        // $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-        // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // return $pdo;
-        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $cleardb_server = $cleardb_url["host"];
-        $cleardb_username = $cleardb_url["user"];
-        $cleardb_password = $cleardb_url["pass"];
-        $cleardb_db = substr($cleardb_url["path"],1);
-        $active_group = 'default';
-        $query_builder = TRUE;
-        // Connect to DB
-        $pdo = new PDO("mysql:host=".$cleardb_server."; dbname=".$cleardb_db, $cleardb_username, $cleardb_password);
-        $pdo->exec('SET NAMES utf8');
+        $pdo = new PDO('mysql:host=127.0.0.1;dbname=zotcoloc;charset=utf8', 'root', '');
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
+        // $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        // $cleardb_server = $cleardb_url["host"];
+        // $cleardb_username = $cleardb_url["user"];
+        // $cleardb_password = $cleardb_url["pass"];
+        // $cleardb_db = substr($cleardb_url["path"],1);
+        // $active_group = 'default';
+        // $query_builder = TRUE;
+        // // Connect to DB
+        // $pdo = new PDO("mysql:host=".$cleardb_server."; dbname=".$cleardb_db, $cleardb_username, $cleardb_password);
+        // $pdo->exec('SET NAMES utf8');
+        // $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // return $pdo;
     }
     
 
--- src/libs/session/session.php
+++ src/libs/session/session.php
@@ -12,9 +12,9 @@ if(!isset($_SESSION['role'])){
 if(!isset($_SESSION['id_utilisateur'])){
     $_SESSION['id_utilisateur'] = false;
 }
-$_ENV['URL_APP'] = "https://zotcoloc.herokuapp.com"; //ou en ligne = https://zotcoloc.herokuapp.com 
+$_ENV['URL_APP'] = "http://127.0.0.1:8000"; //ou en ligne = https://zotcoloc.herokuapp.com 
 //ou http://127.0.0.1:8000
 //En ligne :
-putenv("URL_APP=https://zotcoloc.herokuapp.com");
+//putenv("URL_APP=https://zotcoloc.herokuapp.com");
 //En local :
-// putenv("URL_APP=http://127.0.0.1:8000");
+ putenv("URL_APP=http://127.0.0.1:8000");
