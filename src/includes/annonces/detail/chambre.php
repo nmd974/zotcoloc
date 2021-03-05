835e50cd69e59cd3143535a49c6067ce7cc8d4ff
nmd974 <jha.payet@gmail.com>
2021-03-05 11:44:34 +0400

    indentation + modification affichage photo

--- src/includes/annonces/detail/chambre.php
+++ src/includes/annonces/detail/chambre.php
@@ -13,40 +13,44 @@
             <button type="button" class="btn btn-success" name="save_photo_user" data-bs-toggle="modal" data-bs-target="#editPhotoChambre">
                 <i class="fa fa-camera" aria-hidden="true"></i>
             </button>
-            </div>
-            <?php endif;?>
         </div>
-
+        <?php endif;?>
+    </div>
+    
 </div>
 
 <p class="h3"><?= htmlEntities($annonces[1]->titre_chambre) ?></p>
 <div class="d-flex">
-
-<p class="h6 mt-3"> Chambre - <span class="fw-bold"><?= htmlEntities($annonces[1]->surface_chambre) ?>m<sup>2</sup></span></p>
-
+    
+    <p class="h6 mt-3"> Chambre - <span class="fw-bold"><?= htmlEntities($annonces[1]->surface_chambre) ?>m<sup>2</sup></span></p>
+    
 </div>
 <p class="h6 mt-3"><?= htmlEntities($annonces[1]->description_chambre) ?>
 </p>
 <!-- avatar -->
 <div class="d-flex align-items-center mt-3 mb-4">
-<div class="bg-light shadow" style="width:60px; height: 60px; border-radius: 50%;">
-<?php $utilisateurs = Recherches::photo_utilisateur(htmlEntities($annonces[1]->id_utilisateur))?>
-                <?php if(!$utilisateurs[0]):?>
-                <div class="alert alert-danger">Erreur serveur : Impossible de charger le contenu !</div>
-                <?php else :?>
-                    <?php foreach($utilisateurs[1] as $utilisateur):?>
-                <img src="../images/<?= htmlEntities($utilisateur->libelle_photo) ?>" class="rounded-circle shadow-sm p-1" alt="avatar" style="width:60px; height: 60px; border-radius: 50%;">
-                <?php endforeach; ?>
-                    <?php endif; ?>
-            
-</div>
-<div class="ms-3">
-<p class="fw-bold mb-0"><?= ucfirst(htmlEntities($annonces[1]->prenom)) ?></p>
-<p class="mb-0"><?= ucfirst(htmlEntities($annonces[1]->libelle_role)) ?></p>
-</div>
+    <div class="bg-light shadow" style="width:60px; height: 60px; border-radius: 50%;">
+        <?php $utilisateurs = Recherches::photo_utilisateur(htmlEntities($annonces[1]->id_utilisateur))?>
+        <?php if(!$utilisateurs[0]):?>
+        <?php if($annonces[1]->libelle_role == "proprietaire"):?>
+        <img src="../img_default/no-picture-particulier.png" class="rounded-circle shadow-sm p-1" alt="avatar" style="width:60px; height: 60px; border-radius: 50%;">
+        <?php else:?>
+        <img src="../img_default/no-picture-particulier.png" class="rounded-circle shadow-sm p-1" alt="avatar" style="width:60px; height: 60px; border-radius: 50%;">
+        <?php endif;?>
+        <?php else :?>
+        <?php foreach($utilisateurs[1] as $utilisateur):?>
+        <img src="../images/<?= htmlEntities($utilisateur->libelle_photo) ?>" class="rounded-circle shadow-sm p-1" alt="avatar" style="width:60px; height: 60px; border-radius: 50%;">
+        <?php endforeach; ?>
+        <?php endif; ?>
+        
+    </div>
+    <div class="ms-3">
+        <p class="fw-bold mb-0"><?= ucfirst(htmlEntities($annonces[1]->prenom)) ?></p>
+        <p class="mb-0"><?= ucfirst(htmlEntities($annonces[1]->libelle_role)) ?></p>
+    </div>
 </div>
 
-            <!-- titre -->
+<!-- titre -->
 <div class="mb-5 mt-3">
     <div class="border-one ps-1">
         <div class="border-two ps-3">
@@ -57,15 +61,15 @@
 </div>
 <h4 class="text-green h4 mb-4">Chambre</h4>
 <?php $equipements = Recherches::equipementChambre(htmlEntities($annonces[1]->id_chambre))?>
-                <?php if(!$equipements[0]):?>
-                <div class="alert alert-danger">Erreur serveur : Impossible de charger le contenu !</div>
-                <?php else :?>
-                    <div class="d-flex flex-wrap">
-                    <?php foreach($equipements[1] as $equipement):?>
-                    
-                        <p class="me-2"><span class="text-green">#</span><?= htmlEntities($equipement->libelle_equipement)?></p>
-                    
-                    <?php endforeach; ?>
-                    </div>
-                    <?php endif;?>
+<?php if(!$equipements[0]):?>
+<div class="alert alert-danger">Erreur serveur : Impossible de charger le contenu !</div>
+<?php else :?>
+<div class="d-flex flex-wrap">
+    <?php foreach($equipements[1] as $equipement):?>
+    
+    <p class="me-2"><span class="text-green">#</span><?= htmlEntities($equipement->libelle_equipement)?></p>
+    
+    <?php endforeach; ?>
+</div>
+<?php endif;?>
 
--- src/includes/annonces/recherche/resultat.php
+++ src/includes/annonces/recherche/resultat.php
@@ -8,54 +8,54 @@
 <?php include __ROOT__.'/src/controllers/annonces/recherches/getPhotos.php'?>
 
 <div class="m-2 mb-4">
-   
-        <div class="card card-relative shadow-lg border " style="width: 18rem;">
-            <!-- icon coeur en position absolute-->
-            <div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-                    class="bi bi-heart heart" viewBox="0 0 16 16">
-                    <path
-                    d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                </svg>
-            </div>
-        <a href="roommateDetails.php?id=<?= htmlspecialchars($annonce->id_chambre) ?>" class="mode-link">
-            <!-- avatar en position absolute -->
-            <div class="avatar d-flex justify-content-center align-items-center bg-light shadow" style="width:60px; height: 60px; border-radius: 50%;">
-                
-                <?php if(!$utilisateurs):?>
-                    <?php if($annonce->libelle_role == "proprietaire"):?>
-                        <img src="../img_default/no-picture-particulier.png" class="rounded-circle shadow-sm p-1" alt="avatar" style="width:60px; height: 60px; border-radius: 50%;">
-                    <?php else:?>
-                        <img src="../img_default/no-picture-particulier.png" class="rounded-circle shadow-sm p-1" alt="avatar" style="width:60px; height: 60px; border-radius: 50%;">
-                    <?php endif;?>
-                <?php else :?>
-                <?php foreach($utilisateurs as $utilisateur):?>
-                <img src="../images/<?= htmlspecialchars($utilisateur->libelle_photo) ?>" class="rounded-circle shadow-sm p-1" alt="avatar" style="width:60px; height: 60px; border-radius: 50%;">
-                <?php endforeach; ?>
-                <?php endif; ?>
-            </div>
+    
+    <div class="card card-relative shadow-lg border " style="width: 18rem;">
+        <!-- icon coeur en position absolute-->
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+            class="bi bi-heart heart" viewBox="0 0 16 16">
+            <path
+            d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+        </svg>
+    </div>
+    <a href="roommateDetails.php?id=<?= htmlspecialchars($annonce->id_chambre) ?>" class="mode-link">
+        <!-- avatar en position absolute -->
+        <div class="avatar d-flex justify-content-center align-items-center bg-light shadow" style="width:60px; height: 60px; border-radius: 50%;">
             
-            <?php if(!$images):?>
-                <img src="../img_default/no-photo.png" class="d-block w-100" alt="room" style="height:190px;">
+            <?php if(!$utilisateurs):?>
+            <?php if($annonce->libelle_role == "proprietaire"):?>
+            <img src="../img_default/no-picture-particulier.png" class="rounded-circle shadow-sm p-1" alt="avatar" style="width:60px; height: 60px; border-radius: 50%;">
+            <?php else:?>
+            <img src="../img_default/no-picture-particulier.png" class="rounded-circle shadow-sm p-1" alt="avatar" style="width:60px; height: 60px; border-radius: 50%;">
+            <?php endif;?>
             <?php else :?>
-                <?php foreach($images as $image):?>
-                    <img src="../images/<?= htmlspecialchars($image->libelle_photo) ?>" class="d-block w-100" alt="room" style="height:190px;">
-                <?php endforeach; ?>
+            <?php foreach($utilisateurs as $utilisateur):?>
+            <img src="../images/<?= htmlspecialchars($utilisateur->libelle_photo) ?>" class="rounded-circle shadow-sm p-1" alt="avatar" style="width:60px; height: 60px; border-radius: 50%;">
+            <?php endforeach; ?>
             <?php endif; ?>
-            
-            <!-- descriptif de l'annonce -->
-            <div class="card-body card-rslt">
-                <!-- role -->
-                <span class="badge bg-primary mb-1 letter-space"><?= ucfirst(htmlspecialchars($annonce->libelle_role, ENT_QUOTES)) ?></span>
-                <!-- titre de l'annonce -->
-                <h5 class="card-title"><?= htmlspecialchars(substr($annonce->titre_chambre, 0 , 20)) ?>...</h5>
-                <p class="card-text fw-bold"><?= htmlspecialchars($annonce->libelle_ville) ?></p>
-                <p class="card-text">Disponible le : <?= (new DateTime(htmlspecialchars($annonce->date_disponibilite).'00:00:00'))->format('d/m/Y') ?></p>
-                <p class="card-text"><span class="fw-bold h4"><?= htmlspecialchars($annonce->loyer) ?> €</span> par mois</p>
-            </div>
-            
         </div>
-    </a>
+        
+        <?php if(!$images):?>
+        <img src="../img_default/no-photo.png" class="d-block w-100" alt="room" style="height:190px;">
+        <?php else :?>
+        <?php foreach($images as $image):?>
+        <img src="../images/<?= htmlspecialchars($image->libelle_photo) ?>" class="d-block w-100" alt="room" style="height:190px;">
+        <?php endforeach; ?>
+        <?php endif; ?>
+        
+        <!-- descriptif de l'annonce -->
+        <div class="card-body card-rslt">
+            <!-- role -->
+            <span class="badge bg-primary mb-1 letter-space"><?= ucfirst(htmlspecialchars($annonce->libelle_role, ENT_QUOTES)) ?></span>
+            <!-- titre de l'annonce -->
+            <h5 class="card-title"><?= htmlspecialchars(substr($annonce->titre_chambre, 0 , 20)) ?>...</h5>
+            <p class="card-text fw-bold"><?= htmlspecialchars($annonce->libelle_ville) ?></p>
+            <p class="card-text">Disponible le : <?= (new DateTime(htmlspecialchars($annonce->date_disponibilite).'00:00:00'))->format('d/m/Y') ?></p>
+            <p class="card-text"><span class="fw-bold h4"><?= htmlspecialchars($annonce->loyer) ?> €</span> par mois</p>
+        </div>
+        
+    </div>
+</a>
 </div>
 <?php endforeach; ?>
 <?php endif; ?>
